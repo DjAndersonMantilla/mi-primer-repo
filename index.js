827611207c6987ba05699daa87f811e6eb2ebcfb
Anderson <andermanquin@hotmail.com>
2025-09-18 19:46:38 -0500

    prueba2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hola Mundo!'));
+app.get('/', (req, res) => res.send('hola mundo'));
 app.listen(3000);
 
