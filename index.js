be5e31729d0027194e0c4b9350fd202e0df1acb7
Anderson <andermanquin@hotmail.com>
2025-09-23 20:26:51 -0500

    3 c

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('hola mundo'));
+app.get('/', (req, res) => res.send('hola mundito'));
 app.listen(3000);
 
